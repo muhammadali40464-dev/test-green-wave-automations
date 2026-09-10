d35accb42573b05340f624dd2a4a187a3a52af4e
muhammadali40464-dev <muhammad.ali40464@gmail.com>
2026-09-10 12:37:15 +0500

    Add route for ChatbotPricePakistan page

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import ChatbotGuideSmallBusiness from "./pages/blog/ChatbotGuideSmallBusiness";
 import WhatsAppAPIvsRegular from "./pages/blog/WhatsAppAPIvsRegular";
 import WhatsAppAPIGuide from "./pages/blog/WhatsAppAPIGuide";
 import ChatbotAutomationGuide from "./pages/blog/ChatbotAutomationGuide";
+import ChatbotPricePakistan from "./pages/blog/ChatbotPricePakistan";
 import CustomChatbotDevelopment from "./pages/services/CustomChatbotDevelopment";
 import WhatsAppChatbotDevelopment from "./pages/services/WhatsAppChatbotDevelopment";
 import Legal from "./pages/Legal";
@@ -125,6 +126,7 @@ const App = () => {
             <Route path="/terms-of-service" element={<TermsOfService />} />
             <Route path="/cookie-policy" element={<CookiePolicy />} />
             <Route path="/blog/chatbot-automation-guide-pakistan" element={<ChatbotAutomationGuide />} />
+            <Route path="/blog/chatbot-price-pakistan" element={<ChatbotPricePakistan />} />
             <Route path="/blog/solar-companies-whatsapp-automation-pakistan" element={<SolarAutomation />} />
             <Route path="/blog/visa-consultancy-whatsapp-automation-urdu" element={<VisaConsultancy />} />
             <Route path="/blog/how-chatbots-work-small-businesses-pakistan" element={<ChatbotGuideSmallBusiness />} />
